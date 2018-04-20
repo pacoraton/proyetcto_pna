966dc3c451c07ff20e523c89354d59d9c266c356
PacoRaton <pacocasino1@hotmail.com>
2018-04-19 23:55:03 -0500

    se cambiaron los archivos de html a php y se separaron el header y el footer

---
+++ includes/templates/footer.php
@@ -0,0 +1,54 @@
+<footer class="side-footer">
+           	  <div class="contenedor clearfix">
+           	   	   <div class="footer-informacion">
+           	   	   	  <h3>Sobre<span> gdlwebcamp</span></h3>
+           	   	   	  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum sint culpa asperiores eos eum quo alias nisi. Soluta, eaque quis in illo veritatis rem quam id non alias. Laboriosam, illo?</p>
+           	   	   </div>
+           	   	   
+           	   	   <div class="ultimos-tweets">
+           	   	     <h3>Ultimos<span> tweets</span></h3>
+           	   	     <ul>
+           	   	     		<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit eos consequuntur non tenetur sit iure facere fugit, nemo. Optio, nisi!</li>
+           	   	     		<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit eos consequuntur non tenetur sit iure facere fugit, nemo. Optio, nisi!</li>
+           	   	     		<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit eos consequuntur non tenetur sit iure facere fugit, nemo. Optio, nisi!</li>
+           	   	     </ul>	
+           	   	   </div> 
+					
+				   <div class="menu">
+				  	 <h3>Redes<span> Sociales</span></h3>	
+				  	 <nav class="redes-sociales">
+                        <a href="#"><i class="fab fa-facebook-square"></i></a>
+                        <a href="#"><i class="fab fa-twitter-square"></i></a>
+                        <a href="#"><i class="fab fa-pinterest"></i></a>
+                        <a href="#"><i class="fab fa-youtube-square"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </nav>
+				   </div>	
+
+		       </div>
+
+		       <p class="copyright">
+		       	 Todos los derechos Reservados GDLWEBCAMP 2018
+		       </p>
+           	
+           </footer>
+
+        <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+         <script src="js/jquery.animateNumber.min.js"></script>
+         <script src="js/jquery.countdown.min.js"></script> 
+         <script src="js/jquery.lettering.js"></script>
+        <script src="js/main.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB7UmiwOl-LKZo0b2aohNNcJTnTA02q43o&callback=initMap"
+    async defer></script>
+
+        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+        <script>
+            window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+            ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+        </script>
+        <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+    </body>
+</html>
---
+++ includes/templates/header.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="apple-touch-icon" href="icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/fontawesome-all.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald|PT+Sans" rel="stylesheet"> 
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+        <header class="site-header">
+            <div class="hero">
+                <div class="contenido-header">
+                    <nav class="redes-sociales">
+                        <a href="#"><i class="fab fa-facebook-square"></i></a>
+                        <a href="#"><i class="fab fa-twitter-square"></i></a>
+                        <a href="#"><i class="fab fa-pinterest"></i></a>
+                        <a href="#"><i class="fab fa-youtube-square"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </nav>
+                    <div class="informacion-evento">
+
+                        <div class="clearfix">
+                            <p class="fecha"><i class="fas fa-calendar-alt"></i>10-12 Dic</p>
+                            <p class="ciudad"><i class="fas fa-map-marker-alt"></i>Guadalajara,MX</p>
+                        </div>    
+                        <h1 class="nombre-sitio">GdlWebCamp</h1>
+                    <p class="slogan">La mejor conferencia de <span>diseño web</span></p>
+                    </div>  
+                </div><!--.contenido-header-->
+            </div><!--.hero-->
+            
+        </header> 
+
+        <div class="barra">
+            <div class="contenedor clearfix">
+                <div class="logo">
+                    <a href="index.php">
+                        <img src="img/logo.svg" alt="Logo_gdlwebcamp">  
+                     </a> 
+                </div>  
+               
+                <div class="menu-movil">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                
+                <nav class="navegacion-principal clearfix">
+                    <a href="conferencia.php">Conferencia</a>
+                    <a href="#">Calendario</a>
+                    <a href="#">Invitados</a>
+                    <a href="registro.php">Reservaciones</a>              
+                </nav>   
+            </div><!--.contenedor -->
+        </div><!--.barra -->
